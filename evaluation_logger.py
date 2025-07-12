74a91410b555ac5d028498ffc782ca58b2e18adc
aatif-dawawala <aatifdawawala@gmail.com>
2025-07-12 16:08:58 -0500

    add mongodb connection

--- evaluation_logger.py
+++ evaluation_logger.py
@@ -1,7 +1,18 @@
 import pandas as pd
 import os
 import json
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+uri = os.getenv("uri")
+
+client = MongoClient(uri, server_api=ServerApi("1"))
+
+try:
+    client.admin.command('ping')
+    print("Pinged!")
+except Exception as e:
+    print(e)
 
 def append_example(prompt: str, response: dict, path: str = "eval_dataset.csv"):
 
