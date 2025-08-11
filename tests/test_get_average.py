b3b0c3a34401d03b4e20e69087cc59cfabacca8e
aatif-dawawala <aatifdawawala@gmail.com>
2025-08-11 10:45:54 -0500

    update tests

--- tests/test_get_average.py
+++ tests/test_get_average.py
@@ -7,9 +7,10 @@ sys.path.append(parent_dir)
 
 from utilities.print_average import get_average
 
+eval_results = "tests/eval_results.csv"
 
 def test_get_average():
-    average = get_average()
+    average = get_average(eval_results)
 
     assert average == 4.339285714285714
 
