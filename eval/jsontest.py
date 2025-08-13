401804acc482ffef2e41ba914a938713d26a5d34
aatif-dawawala <aatifdawawala@gmail.com>
2025-08-13 09:20:31 -0500

    Dynamically load JSON content from metrics.json into the Gemini eval.

--- eval/jsontest.py
+++ eval/jsontest.py
@@ -1,6 +1,10 @@
 import json
 import sys
 import os
+import vertexai
+import pandas as pd
+from vertexai.evaluation import EvalTask, PointwiseMetric, PointwiseMetricPromptTemplate
+from google.cloud import aiplatform
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
@@ -9,9 +13,21 @@ sys.path.append(parent_dir)
 
 metrics_path = "eval/metrics.json"
 
+metrics = []
+
 with open(metrics_path, 'r') as f:
     metric_data = json.load(f)
 
-for temp in metric_data:
-    print(metric_data[temp]["criteria"])
+for i in range(len(metric_data)):
+    
+    metrics.append(PointwiseMetric(
+            metric=metric_data[i][0],
+            metric_prompt_template=PointwiseMetricPromptTemplate(
+                criteria=metric_data[i][1],
+                rating_rubric=metric_data[i][2]
+            )
+        )
+    )
 
+print(metrics)
+    
