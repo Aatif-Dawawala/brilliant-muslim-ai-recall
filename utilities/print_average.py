999e382c580cacae3764460d3717018ad1c4a15c
Aatif-Dawawala <aatifdawawala@gmail.com>
2025-08-19 05:26:45 -0500

    Fix print_average utility. Remove jsontest.

--- utilities/print_average.py
+++ utilities/print_average.py
@@ -1,24 +1,42 @@
 import csv
 import os 
 import sys
+import json
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
 def get_average(file_name = "data/eval_results.csv"):
+    metrics_path = "data/metrics.json"
+    metrics = []
+    averages = {}
     scores = []
     sum = 0
 
+    with open(metrics_path, 'r') as f:
+        metric_data = json.load(f)
+
+    for i in range(len(metric_data)):
+        metrics.append(metric_data[i][0] + "/score")
+    print(metrics)
+
     with open(file_name, 'r', encoding="utf-8") as file: 
         dict_reader = csv.DictReader(file)
-        for row_dict in dict_reader:
-            scores.append(float(row_dict["custom_text-quality/score"]))
+        csv_rows = list(dict_reader)
+        for item in metrics:
+            for row_dict in csv_rows:
+                if row_dict[item] != "":
+                    scores.append(float(row_dict[item]))
+            for temp in scores:
+                sum += temp
 
-    for temp in scores:
-        sum += temp
+                averages[item] = sum/len(scores)
+
+            scores = []
+            sum = 0
         
-    return sum/len(scores)
+    return averages
 
 if __name__ == "__main__":
     print(get_average())
